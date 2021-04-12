da3b0560b82618849471abb4849bbffd4f6188de
svatos331 <54244300+svatos331@users.noreply.github.com>
2021-04-12 09:07:39 +0400

    12.04.2021
    Animation automatic gas distribution station polygon

---
+++ js.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    document.getElementById('svg_14').addEventListener('mouseover', function(){
+        document.getElementById('svg_8').setAttribute('style', ' y: -20;')
+        document.getElementById('commentSvg1').setAttribute('style', 'opacity: 1;')
+
+
+
+
+        
+
+        document.getElementById('commentSvg1').children[0].setAttribute('style', 'transform: translate(0px, 61px);')
+    })
+    
+    document.getElementById('svg_14').addEventListener('mouseout', function(){
+        document.getElementById('svg_8').setAttribute('style', ' y: 0;')
+        document.getElementById('commentSvg1').setAttribute('style', 'opacity: 0;')
+        document.getElementById('commentSvg1').children[0].setAttribute('style', 'transform: translate(0px, 81px);')
+    })
+
+    document.getElementById('svg_14').addEventListener('click', function(){
+        window.location.href = 'http://www.google.com';
+    })
+    
+
+
+
+    document.getElementById('svg_16').addEventListener('mouseover', function(){
+        document.getElementById('svg_5').setAttribute('style', 'y: -20;')
+
+        document.getElementById('commentSvg2').setAttribute('style', 'opacity: 1;')
+        document.getElementById('commentSvg2').children[0].setAttribute('style', 'transform: translate(0px, 61px);')
+    })
+    
+    document.getElementById('svg_16').addEventListener('mouseout', function(){
+        document.getElementById('svg_5').setAttribute('style', ' y: 0;')
+
+        document.getElementById('commentSvg2').setAttribute('style', 'opacity: 0;')
+        document.getElementById('commentSvg2').children[0].setAttribute('style', 'transform: translate(0px, 81px);')
+
+
+    })
+    document.getElementById('svg_16').addEventListener('click', function(){
+        window.location.href = 'http://www.google.com';
+    })
+
+
+
+    // transform: translate(0px, -20px)
+
+
+
+    document.getElementById('svg_17123').addEventListener('mouseover', function(){
+        document.getElementById('svg_3').setAttribute('style', ' y: -20;')
+
+        document.getElementById('commentSvg5').setAttribute('style', 'opacity: 1;')
+        document.getElementById('commentSvg5').children[0].setAttribute('style', 'transform: translate(0px, 61px);')
+
+    })
+    
+    document.getElementById('svg_17123').addEventListener('mouseout', function(){
+        document.getElementById('svg_3').setAttribute('style', ' y: 0;')
+
+        document.getElementById('commentSvg5').setAttribute('style', 'opacity: 0;')
+        document.getElementById('commentSvg5').children[0].setAttribute('style', 'transform: translate(0px, 81px);')
+
+    })
+    document.getElementById('svg_17123').addEventListener('click', function(){
+        window.location.href = 'http://www.google.com';
+    })
+
+
+    
+
+
+
+
+
+    
+
+
+    document.getElementById('svg_21').addEventListener('mouseover', function(){
+        document.getElementById('svg_4').setAttribute('style', ' y: -20;')
+        document.getElementById('commentSvg4').setAttribute('style', 'opacity: 1;')
+        document.getElementById('commentSvg4').children[0].setAttribute('style', 'transform: translate(0px, 61px);')
+    })
+    document.getElementById('svg_21').addEventListener('mouseout', function(){
+        document.getElementById('svg_4').setAttribute('style', ' y: 0;')
+        document.getElementById('commentSvg4').setAttribute('style', 'opacity: 0;')
+        document.getElementById('commentSvg4').children[0].setAttribute('style', 'transform: translate(0px, 81px);')
+
+
+
+    })
+    document.getElementById('svg_21').addEventListener('click', function(){
+        window.location.href = 'http://www.google.com';
+    })
+
+
+
+    document.getElementById('svg_19').addEventListener('mouseover', function(){
+        document.getElementById('svg_6').setAttribute('style', ' y: -20;')
+        document.getElementById('commentSvg3').setAttribute('style', 'opacity: 1;')
+        document.getElementById('commentSvg3').children[0].setAttribute('style', 'transform: translate(0px, 61px);')
+    })
+    
+    document.getElementById('svg_19').addEventListener('mouseout', function(){
+        document.getElementById('svg_6').setAttribute('style', ' y: 0;')
+
+        document.getElementById('commentSvg3').setAttribute('style', 'opacity: 0;')
+        document.getElementById('commentSvg3').children[0].setAttribute('style', 'transform: translate(0px, 81px);')
+
+    })
+    document.getElementById('svg_19').addEventListener('click', function(){
+        window.location.href = 'http://www.google.com';
+    })
+    
+    
+
+    
+    })
